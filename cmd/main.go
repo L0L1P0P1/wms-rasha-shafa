8e4ede592a4faeaec668ed02f79a0f1a3b93a89f
L0L1P0P1 <behradbadeli@gmail.com>
2026-09-10 18:45:02 +0330

    sqlc

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+)
 
 func main() {
 	fmt.Println("Hello World!")
+
+	server := http.NewServeMux()
+	server.HandleFunc("/api/ping", ping)
+
+	port := ":8888"
+
+	if err := http.ListenAndServe(port, server); err != nil {
+		log.Fatalf("Couldn't setup server %v", err)
+	}
+}
+
+func ping(w http.ResponseWriter, r *http.Request) {
+	var pong struct {
+		Pong string `json:"pong"`
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(pong)
 }
---
+++ db/queries/skus.sql
@@ -0,0 +1,2 @@
+-- name: GetSomething :one
+SELECT * FROM stock_keeping_units LIMIT 1;
